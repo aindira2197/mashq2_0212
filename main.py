4333b59dc06f5b852376dc031b68e4d1c415b6b4
Azadova Indira <aindira2197@gmail.com>
2025-12-02 15:41:37 +0500

    Implement example lambda functions in main.py
    Add multiple lambda functions for various examples.

---
+++ main.py
@@ -0,0 +1,18 @@
+#1 - misol
+have = lambda soz: "a bor" if "a" in soz else " a yoq"
+print(have("Apple"))
+
+#2 - misol
+bosh = lambda soz: soz.title()
+print(bosh('apple'))
+
+#3 - misol
+farq = lambda a, b: a-b if a > b else b-a
+print(farq(12, 34))
+
+#4 - misol
+farqi = lambda a, b: True if a == b else False
+print(farqi(23, 9))
+
+#5 - misol
+boliw = lambda son: son // 2
